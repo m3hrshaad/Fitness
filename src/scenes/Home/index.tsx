08d774f6f7b88692865774147c6402057e738a32
unknown <mshd.p.22@gmail.com>
2025-10-04 10:19:31 +0330

    main section done

--- src/scenes/Home/index.tsx
+++ src/scenes/Home/index.tsx
@@ -7,6 +7,8 @@ import SponsorRedBull from "../../assets/SponsorRedBull.png"
 import SponsorForbes from "../../assets/SponsorForbes.png"
 import SponsorFortune from "../../assets/SponsorFortune.png"
 import AnchorLink from "react-anchor-link-smooth-scroll"
+import { motion } from "framer-motion"
+
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void
@@ -15,24 +17,34 @@ type Props = {
 const Home = ({ setSelectedPage }: Props) => {
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)")
   return (
-    <section id="home" className="gap-16 bg-gray-100 py-10 md:h-full md:pb-0">
+    <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
       {/* Image and main header */}
-      <div>
+      <motion.div 
+        onViewportEnter={() => setSelectedPage(SelectedPage.Home)}
+        className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6">
         {/* main header */}
-        <div>
+        <div className="z-10 mt-32 md:basis-3/5">
           {/* headings */}
-          <div>
-            <div>
-              <div>
+          <motion.div initial='hidden' whileInView='visible' viewport={{ once: true, amount:0.5 }} 
+          transition={{ duration: 0.5}} variants={{ hidden: {opacity: 0, x:-100 }, visible: { opacity: 1, x:0 } }}
+            className="md:-mt-20 ">
+            <div className="relative">
+              <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext">
                 <img src={HomePageText} alt="home-page-text" />
               </div>
             </div>
 
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia odio exercitationem magni eaque repellendus fuga, sunt facilis voluptatem aperiam. Autem quibusdam fugit odit praesentium, nisi iusto voluptates doloremque quasi ut.</p>
-          </div>
+              <p className="mt-8 text-sm ">
+              Unrivaled Gym. Unparalleled Training Fitness Classes. World Class
+              Studios to get the Body Shapes That you Dream of.. Get Your Dream
+              Body Now.
+            </p>          
+          </motion.div>
 
           {/* Actions */}
-          <div>
+          <motion.div initial='hidden' whileInView='visible' viewport={{ once: true, amount:0.5 }} 
+              transition={{ delay:0.2, duration: 0.5}} variants={{ hidden: {opacity: 0, x:-100 }, visible: { opacity: 1, x:0 } }}
+              className="mt-8 flex items-center gap-8">
             <ActionButton setSelectedPage={setSelectedPage}>
               Join Now
             </ActionButton>
@@ -42,20 +54,25 @@ const Home = ({ setSelectedPage }: Props) => {
             >
               <p>Learn More</p>
             </AnchorLink>
-          </div>
+          </motion.div>
         </div>
 
         {/* Image */}
-        <div>
+        <div className="flex basis-3/5 justify-center md:z-10
+        md:ml-40 md:mt-16 md:justify-items-end">
           <img src={HomePageGraphic} alt="home-pageGraphic" />
         </div>
-      </div>
+      </motion.div>
+
+      {/* Sponsors */}
       {isAboveMediumScreens && (
-        <div>
-          <div>
-            <img src={SponsorRedBull} alt="redbull-sponsor" />
-            <img src={SponsorForbes} alt="forbes-sponsor" />
-            <img src={SponsorFortune} alt="fortune-sponsor" />
+        <div className="h-[150px] w-full bg-primary-100 py-10">
+          <div className="mx-auto w-5/6">
+            <div className="flex w-3/5 items-center justify-between gap-8">
+              <img src={SponsorRedBull} alt="redbull-sponsor" />
+              <img src={SponsorForbes} alt="forbes-sponsor" />
+              <img src={SponsorFortune} alt="fortune-sponsor" />
+            </div>
           </div>
         </div>
       )}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
@@ -14,11 +15,10 @@ module.exports = {
         "secondary-400": "#FFCD5B",
         "secondary-500": "#FFC132",
       },
-      backgroundImage: (theme) => ({
-        "gradient-yellowred":
-          "linear-gradient(90deg, #FF616A 0%, #FFC837 100%)",
+      backgroundImage: {
+        "gradient-yellowred": "linear-gradient(90deg, #FF616A 0%, #FFC837 100%)",
         "mobile-home": "url('./assets/HomePageGraphic.png')",
-      }),
+      },
       fontFamily: {
         dmsans: ["DM Sans", "sans-serif"],
         montserrat: ["Montserrat", "sans-serif"],
@@ -37,5 +37,6 @@ module.exports = {
     },
   },
   plugins: [],
-};
- 
+} satisfies Config;
+
+
