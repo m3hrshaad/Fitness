4c93cb0c0eb25f1980042f2380461e8a1e1a723b
unknown <mshd.p.22@gmail.com>
2025-10-08 10:39:45 +0330

    add contactUs section

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { SelectedPage } from "./shared/types"
 import Home from "./scenes/Home"
 import Benefits from './scenes/benefits'
 import OurClasses from './scenes/ourClasses'
+import ContactUs from './scenes/contactUs'
 
 
 function App() {
@@ -29,6 +30,7 @@ function App() {
       <Home setSelectedPage={setSelectedPage}/>
       <Benefits setSelectedPage={setSelectedPage}/>
       <OurClasses setSelectedPage={setSelectedPage}/>
+      <ContactUs setSelectedPage={setSelectedPage}/>
     </div>
   )
 }
